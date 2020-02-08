95fa590d121c32b0dc0c13943d1bc5983e544f9d
Marcelo <59406899+mrsants@users.noreply.github.com>
2020-02-07 23:00:36 -0300

    Configurando estrutura

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
+"use strict";
+
+const path = require("path");
+
 module.exports = {
-    
-}
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {},
+  devtool: "source-map",
+  plugins: []
+};
